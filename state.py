4bdc5954616db26763a072538e34b2a815de161d
jonathonherbert <jonathon.herbert@guardian.co.uk>
2022-01-23 21:09:16 +0000

    Python dates, are kinda great

--- state.py
+++ state.py
@@ -1,8 +1,9 @@
 import audio
+from utime import gmtime, mktime, localtime
 
 
 def getShortDateTime(date):
-    return date[2] + '/' + date[1] + '/' + date[0] + ' ' + date[3] + ':' + date[4] + ':' + date[5]
+    return '{}/{}/{} {}:{}:{}'.format(date[2], date[1], date[0], date[3], date[4], date[5])
 
 
 class BaseState:
@@ -40,40 +41,50 @@ class IdleUIState(BaseState):
 
 class SetEndTimeDDState(BaseState):
     def __init__(self, starterDate, fridgeIsOn):
-        super(starterDate, fridgeIsOn)
+        if (starterDate):
+            super().__init__(starterDate, fridgeIsOn)
+        else:
+            now = gmtime()
+            oneDayAhead = localtime(mktime((
+                now[0], now[1], now[2] + 1, now[3], now[4], now[5], 0, 0)))
+            super().__init__(oneDayAhead, fridgeIsOn)
 
     def incr(self):
-        self.starterDate = self.starterDate[:2] + \
-            (self.starterDate[2] + 1,) + self.starterDate[3:]
+        date = self.starterDate
+        self.starterDate = localtime(mktime((
+            date[0], date[1], date[2] + 1, date[3], date[4], date[5], date[6], date[7])))
         return self
 
     def decr(self):
-        self.starterDate = self.starterDate[:2] + \
-            (self.starterDate[2] - 1,) + self.starterDate[3:]
+        date = self.starterDate
+        self.starterDate = localtime(mktime((
+            date[0], date[1], date[2] - 1, date[3], date[4], date[5], date[6], date[7])))
         return self
 
     def enter(self):
         return SetEndTimeHHState(self.starterDate, self.fridgeIsOn)
 
     def display(self):
-        return 'Set day: ' + self.starterDate[2]
+        return 'Set day: {}'.format(self.starterDate[2])
 
 
 class SetEndTimeHHState(BaseState):
     def __init__(self, starterDate, fridgeIsOn):
-        super(starterDate, fridgeIsOn)
+        super().__init__(starterDate, fridgeIsOn)
 
     def display(self):
-        return 'Set hour: ' + self.starterDate[3]
+        return 'Set hour: {}'.format(self.starterDate[3])
 
     def incr(self):
-        self.starterDate = self.starterDate[:3] + \
-            (self.starterDate[3] + 1,) + self.starterDate[4:]
+        date = self.starterDate
+        self.starterDate = localtime(mktime((
+            date[0], date[1], date[2], date[3] + 1, date[4], date[5], date[6], date[7])))
         return self
 
     def decr(self):
-        self.starterDate = self.starterDate[:3] + \
-            (self.starterDate[3] - 1,) + self.starterDate[4:]
+        date = self.starterDate
+        self.starterDate = localtime(mktime((
+            date[0], date[1], date[2], date[3] + 1, date[4], date[5], date[6], date[7])))
         return self
 
     def enter(self):
